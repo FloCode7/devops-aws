b71283eb7a4e3e0bdcc29e14ab87d42362fa5ba8
FloCode7 <flo.f3@hotmail.fr>
2024-09-02 17:05:46 +0200

    test login component events fixed

--- src/test/Login.test.js
+++ src/test/Login.test.js
@@ -1,15 +1,15 @@
-import { render } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import Login from "../components/Login";
 
 test("renders login component", () => {
   render(<Login />);
 
-  const emailInput = screen.getByLabelText("Email : ");
-  const passwordInput = screen.getByLabelText("Password : ");
+  const emailInput = screen.getByLabelText(/email/i);
+  const passwordInput = screen.getByLabelText(/password/i);
 
   fireEvent.change(emailInput, { target: { value: "test@gmail.com" } });
-  fireEvent.change(passwordInput, { target: { value: "test123" } });
+  fireEvent.change(passwordInput, { target: { value: "onepassword" } });
 
   expect(emailInput.value).toBe("test@gmail.com");
-  expect(passwordInput.value).toBe("test123");
+  expect(passwordInput.value).toBe("onepassword");
 });
